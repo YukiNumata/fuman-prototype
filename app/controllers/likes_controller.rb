5389f8b00b48cfac35bb91d17c005749d9914c1a
中沢冬芽 <nakazawatoga@nakazawatouganoMacBook-Pro.local>
2018-06-19 16:37:48 +0900

    k

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,9 @@
 class LikesController < ApplicationController
   def create
     @like = Like.create(user_id:cookies.signed[:user_id], fuman_id:params[:id])
-    @fuman=Fuman.find(params[:id])
+    @fuman=Fuman.find(params[:fuman_id])
     @fuman.like+=1
-    render_to fumans_path
+    @fuman.save
+    redirect_to fumans_path, :method=>:get
   end
 end
